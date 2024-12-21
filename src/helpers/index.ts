a0664ea077c02ff8ab6f5d6ab5b99e58fadbed0d
Ariel Hurtado <arielhurtado388@gmail.com>
2024-12-20 20:36:22 -0500

    Listar gastos

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -4,3 +4,14 @@ export function formatCurrency(amount: number) {
     currency: "USD",
   }).format(amount);
 }
+
+export function formatDate(date: string): string {
+  const dateObj = new Date(date);
+  const options: Intl.DateTimeFormatOptions = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+  return new Intl.DateTimeFormat("es-ES", options).format(dateObj);
+}
