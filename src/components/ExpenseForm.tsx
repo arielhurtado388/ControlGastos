99fccf08a8d488f1017e849b30398e8149562253
Ariel Hurtado <arielhurtado388@gmail.com>
2024-12-22 00:52:16 -0500

    Editar gasto y localstorage de budget y expenses listo

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, FormEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useEffect, useState } from "react";
 import { categories } from "../data/categories";
 import DatePicker from "react-date-picker";
 import "react-date-picker/dist/DatePicker.css";
@@ -17,7 +17,16 @@ export default function ExpenseForm() {
 
   const [error, setError] = useState("");
 
-  const { dispatch } = useBudget();
+  const { dispatch, state } = useBudget();
+
+  useEffect(() => {
+    if (state.editingId) {
+      const editingExpense = state.expenses.filter(
+        (currentExpense) => currentExpense.id === state.editingId
+      )[0];
+      setExpense(editingExpense);
+    }
+  }, [state.editingId]);
 
   const handleChange = (
     e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>
@@ -44,8 +53,17 @@ export default function ExpenseForm() {
       setError("Todos los campos son obligatorios");
       return;
     }
-    // Agregar un nuevo gasto
-    dispatch({ type: "add-expense", payload: { expense } });
+    // Agregar o actualizar el gasto
+
+    if (state.editingId) {
+      dispatch({
+        type: "update-expense",
+        payload: { expense: { id: state.editingId, ...expense } },
+      });
+    } else {
+      dispatch({ type: "add-expense", payload: { expense } });
+    }
+
     // Reiniciar el state
     setExpense({
       amount: 0,
@@ -58,7 +76,7 @@ export default function ExpenseForm() {
   return (
     <form action="" className="space-y-5" onSubmit={handleSubmit}>
       <legend className="uppercase text-center text-2xl font-black border-b-4 py-2 border-blue-500">
-        Nuevo gasto
+        {state.editingId ? "Editar gasto" : "Nuevo gasto"}
       </legend>
 
       {error && <ErrorMessage>{error} </ErrorMessage>}
@@ -127,7 +145,7 @@ export default function ExpenseForm() {
       <input
         type="submit"
         className="bg-blue-600 cursor-pointer w-full p-2 text-white uppercase font-bold rounded-lg"
-        value={"Registrar gasto"}
+        value={state.editingId ? "Guardar cambios" : "Registrar gasto"}
       />
     </form>
   );
--- src/reducers/budget-reducer.ts
+++ src/reducers/budget-reducer.ts
@@ -9,18 +9,32 @@ export type BudgetActions =
   | { type: "show-modal" }
   | { type: "close-modal" }
   | { type: "add-expense"; payload: { expense: DraftExpense } }
-  | { type: "remove-expense"; payload: { id: Expense["id"] } };
+  | { type: "remove-expense"; payload: { id: Expense["id"] } }
+  | { type: "get-expense-by-id"; payload: { id: Expense["id"] } }
+  | { type: "update-expense"; payload: { expense: Expense } };
 
 export type BudgetState = {
   budget: number;
   modal: boolean;
   expenses: Expense[];
+  editingId: Expense["id"];
+};
+
+const initialBudget = (): number => {
+  const localStorageBudget = localStorage.getItem("budget");
+  return localStorageBudget ? +localStorageBudget : 0;
+};
+
+const localStorageExpenses = (): Expense[] => {
+  const expenses = localStorage.getItem("expenses");
+  return expenses ? JSON.parse(expenses) : [];
 };
 
 export const initialState: BudgetState = {
-  budget: 0,
+  budget: initialBudget(),
   modal: false,
-  expenses: [],
+  expenses: localStorageExpenses(),
+  editingId: "",
 };
 
 const createExpense = (drafExpense: DraftExpense): Expense => {
@@ -47,6 +61,7 @@ export const budgetReducer = (state: BudgetState, action: BudgetActions) => {
     return {
       ...state,
       modal: false,
+      editingId: "",
     };
   }
   if (action.type === "add-expense") {
@@ -64,6 +79,25 @@ export const budgetReducer = (state: BudgetState, action: BudgetActions) => {
       expenses: state.expenses.filter(
         (expense) => expense.id !== action.payload.id
       ),
+    };
+  }
+  if (action.type === "get-expense-by-id") {
+    return {
+      ...state,
+      editingId: action.payload.id,
+      modal: true,
+    };
+  }
+  if (action.type === "update-expense") {
+    return {
+      ...state,
+      expenses: state.expenses.map((expense) =>
+        expense.id === action.payload.expense.id
+          ? action.payload.expense
+          : expense
+      ),
+      modal: false,
+      editingId: "",
     };
   }
   return state;
