cd51d5591877943a092994cdd7c44875d13ce620
Ariel Hurtado <arielhurtado388@gmail.com>
2024-12-22 21:37:04 -0500

    Tracker con grafica dinamica y reset app lista

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -17,7 +17,9 @@ export default function ExpenseForm() {
 
   const [error, setError] = useState("");
 
-  const { dispatch, state } = useBudget();
+  const [previousAmount, setPreviousAmount] = useState(0);
+
+  const { dispatch, state, remainingBudget } = useBudget();
 
   useEffect(() => {
     if (state.editingId) {
@@ -25,6 +27,7 @@ export default function ExpenseForm() {
         (currentExpense) => currentExpense.id === state.editingId
       )[0];
       setExpense(editingExpense);
+      setPreviousAmount(editingExpense.amount);
     }
   }, [state.editingId]);
 
@@ -53,6 +56,14 @@ export default function ExpenseForm() {
       setError("Todos los campos son obligatorios");
       return;
     }
+
+    // Validar que no me pase el presupuesto
+
+    if (expense.amount - previousAmount > remainingBudget) {
+      setError("Prespuesto rebasado");
+      return;
+    }
+
     // Agregar o actualizar el gasto
 
     if (state.editingId) {
@@ -71,6 +82,7 @@ export default function ExpenseForm() {
       category: "",
       date: new Date(),
     });
+    setPreviousAmount(0);
   };
 
   return (
--- src/reducers/budget-reducer.ts
+++ src/reducers/budget-reducer.ts
@@ -11,7 +11,8 @@ export type BudgetActions =
   | { type: "add-expense"; payload: { expense: DraftExpense } }
   | { type: "remove-expense"; payload: { id: Expense["id"] } }
   | { type: "get-expense-by-id"; payload: { id: Expense["id"] } }
-  | { type: "update-expense"; payload: { expense: Expense } };
+  | { type: "update-expense"; payload: { expense: Expense } }
+  | { type: "reset-app" };
 
 export type BudgetState = {
   budget: number;
@@ -98,6 +99,13 @@ export const budgetReducer = (state: BudgetState, action: BudgetActions) => {
       ),
       modal: false,
       editingId: "",
+    };
+  }
+  if (action.type === "reset-app") {
+    return {
+      ...state,
+      budget: 0,
+      expenses: [],
     };
   }
   return state;
